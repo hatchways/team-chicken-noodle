ad7fc00573a54ec4391ec0d6cbd4208ba1fa9008
Pursharth Voha <pursharth.vohra@gmail.com>
2021-10-01 13:55:48 -0400

    fix position and resposiveness of profile photo component

--- client/src/components/ProfilePhoto/ProfilePhoto.tsx
+++ client/src/components/ProfilePhoto/ProfilePhoto.tsx
@@ -4,6 +4,7 @@ import useStyles from './useStyles';
 import { Paper } from '@material-ui/core';
 import Avatar from '@material-ui/core/Avatar';
 import { DeleteForeverOutlined } from '@material-ui/icons';
+import ProfileImage from '../../Images/b1f0e680702e811aa8ba333cb19c0e0ea95e8e31.png';
 
 export default function ProfilePhoto(): JSX.Element {
   const classes = useStyles();
@@ -12,10 +13,9 @@ export default function ProfilePhoto(): JSX.Element {
       <Grid
         container
         direction="column"
-        alignItems="center"
         xs={12}
         sm={8}
-        md={7}
+        md={8}
         elevation={6}
         className={classes.profileContainer}
         component={Paper}
@@ -26,7 +26,7 @@ export default function ProfilePhoto(): JSX.Element {
           </Typography>
         </Grid>
         <Grid item>
-          <Avatar alt="Profile Image" src="https://robohash.org/pursharth01@gmail.com.png" className={classes.avatar} />
+          <Avatar alt="Profile Image" src={ProfileImage} className={classes.avatar} />
         </Grid>
         <Grid item className={classes.caption}>
           <Typography variant="caption" align="center" className={classes.captionText}>
@@ -34,11 +34,18 @@ export default function ProfilePhoto(): JSX.Element {
           </Typography>
         </Grid>
         <Grid item>
-          <Button variant="outlined"> Upload a file from your device</Button>
+          <Button variant="outlined" color="inherit" className={classes.uploadBtn}>
+            Upload a file from your device
+          </Button>
         </Grid>
         <Grid item>
-          <DeleteForeverOutlined />
-          <Button variant="text">Delete Photo</Button>
+          <Button
+            variant="text"
+            startIcon={<DeleteForeverOutlined className={classes.deleteIcon} />}
+            className={classes.deleteBtn}
+          >
+            Delete Photo
+          </Button>
         </Grid>
       </Grid>
     </Container>
--- client/src/components/ProfilePhoto/useStyles.ts
+++ client/src/components/ProfilePhoto/useStyles.ts
@@ -3,16 +3,15 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles(() => ({
   root: {
     marginTop: '10rem',
-    display: 'flex',
-    alignContent: 'center',
+    marginBottom: '10rem',
+    alignItems: 'center',
     justifyContent: 'center',
-    marginLeft: 'auto',
-    marginRight: 'auto',
+    minWidth: '20rem',
   },
   profileContainer: {
-    display: 'flex',
-    minWidth: '50rem',
-    minHeight: '40rem',
+    alignItems: 'center',
+    minHeight: '60vh',
+    margin: 'auto',
   },
   contentWrapper: {
     display: 'flex',
@@ -23,21 +22,39 @@ const useStyles = makeStyles(() => ({
   },
   titleText: {
     fontFamily: 'Roboto',
-    fontWeight: 500,
+    fontWeight: 'bolder',
+    fontSize: '1.5rem',
     margin: 'auto',
   },
   avatar: {
-    marginTop: '50px',
-    width: 100,
-    height: 100,
+    marginTop: '4rem',
+    width: 150,
+    height: 150,
   },
   caption: {
     marginTop: '2rem',
-    width: '15rem',
+    color: '#9e9e9e',
+    width: '10rem',
     textAlign: 'center',
-    color: 'primary',
   },
-  captionText: {},
+  captionText: {
+    fontSize: '12px',
+  },
+  uploadBtn: {
+    marginTop: '2rem',
+    color: '#d32f2f',
+    fontSize: '12px',
+    lineHeight: '3rem',
+    minWidth: '20rem',
+  },
+  deleteBtn: {
+    color: '#9e9e9e',
+    marginTop: '2rem',
+    marginBottom: '5rem',
+  },
+  deleteIcon: {
+    color: '#000000',
+  },
 }));
 
 export default useStyles;
