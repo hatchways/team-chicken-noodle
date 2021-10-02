886eaae7d0ba288c8ba200fd0ba6378473c8530c
Pursharth Voha <pursharth.vohra@gmail.com>
2021-10-02 19:09:19 -0400

    upload image to s3 and get url, key for image

--- server/controllers/uploadImage.js
+++ server/controllers/uploadImage.js
@@ -1,9 +1,17 @@
 const asyncHandler = require("express-async-handler");
+const { uploadFile } = require('../utils/s3')
 
 // @route POST /uploadImage
 // @desc Upload image to S3
 // @access Private
 exports.uploadImage = asyncHandler(async (req, res, next) => {
-console.log(req.file)
-  res.status(200).json({});
+  try {
+    const file = req.file;
+    const result = await uploadFile(file);
+    console.log(result);
+    res.status(200).json({ imagePath: `/images/${result.Key}`});
+  }
+  catch (err) {
+    console.log(err);
+  }
 });
--- server/utils/s3.js
+++ server/utils/s3.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const S3 = require('aws-sdk/clients/s3');
+const fs = require('fs');
 
 const bucketName = process.env.AWS_BUCKET_NAME;
 const region = process.env.AWS_BUCKET_REGION;
@@ -11,6 +12,21 @@ const s3 = new S3({
     accessKeyId,
     secretAccessKey
 })
+
+//upload a file to s3
+function uploadFile(file) {
+    const fileStream = fs.createReadStream(file.path);
+    const uploadParams = {
+        Bucket: bucketName,
+        Body: fileStream,
+        Key: file.filename
+    }
+    return s3.upload(uploadParams).promise();
+}
+
+exports.uploadFile = uploadFile;
+
+// download a file from s3
 
 
 
