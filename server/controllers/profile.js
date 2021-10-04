b1ba4d5b6a82527e7180cd99339697ca4f49eeb4
Surafel Seyoum <lesurafel@gmail.com>
2021-10-04 15:05:07 -0400

    Update Profile controllers
    Update Profile controllers base on the comments.

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -1,102 +1,80 @@
-const Profile = require("../models/Profile");
-const asyncHandler = require("express-async-handler");
+const Profile = require('../models/Profile');
+const asyncHandler = require('express-async-handler');
 
 // @route POST /profile/create
 // @desc create a profile
 // @access Private
 exports.createProfile = asyncHandler(async (req, res, next) => {
-    const userId = req.user.id;
+  const userId = req.user.id;
 
-    const profileExists = await Profile.findOne({ userId:userId });
-    if (profileExists) {
-        res.status(400);
-        throw new Error("A user profile already registered");
-    }
+  const profileExists = await Profile.findOne({ userId: userId });
+  if (profileExists) {
+    res.status(400);
+    throw new Error('A user profile already registered');
+  }
 
-    const {
-        firstName, lastName, description, gender,
-        address, birthDate, phoneNumber, isAvailable, availability
-    } = req.body
-    
-    const profile = await Profile.create({
-        userId,
-        firstName,
-        lastName,
-        description,
-        gender,
-        birthDate,
-        phoneNumber,
-        address,
-        isAvailable,
-        availability,
-    })
+  const {
+    firstName,
+    lastName,
+    description,
+    gender,
+    address,
+    birthDate,
+    phoneNumber,
+    isAvailable,
+    availability,
+  } = req.body;
 
-    if(!profile) {
-        res.status(400);
-        throw new Error("Invalid request data");
-    }
-    
-     res.status(201).json({
-        success: true,
-        data: profile
-    });
+  const profile = await Profile.create({
+    userId,
+    firstName,
+    lastName,
+    description,
+    gender,
+    birthDate,
+    phoneNumber,
+    address,
+    isAvailable,
+    availability,
   });
+
+  res.status(201).json({ profile });
+});
 
 // @route GET /profile/
 // @desc find all profiles
 // @access Private
-exports.findAllProfiles= asyncHandler(async (req, res, next) => {
-    const profiles = await Profile.find();
+exports.findAllProfiles = asyncHandler(async (req, res, next) => {
+  const profiles = await Profile.find();
 
-    if(!profiles) {
-        res.status(404);
-        throw new Error(`Cannot find user profile.`);
-    }  
-
-    res.status(200).json({
-      success: true,
-      data: profiles
-    });
+  res.status(200).json({ profiles });
 });
 
 // @route GET /profile/:id
 // @desc find a profile
 // @access Private
 exports.findProfileById = asyncHandler(async (req, res, next) => {
-    const id = req.user.id;
+  const id = req.user.id;
 
-    const profile = await Profile.findOne({ userId:id });
+  const profile = await Profile.findOne({ userId: id });
 
-    if(!profile) {
-        res.status(404);
-        throw new Error(`Cannot find a user profile with this ${id} id.`);
-    }  
-
-    res.status(200).json({
-        success: true,
-        data: profile
-    });
+  res.status(200).json({ profile });
 });
-  
+
 // @route PUT /profile/update
 // @desc update a profile
 // @access Private
 exports.updateProfile = asyncHandler(async (req, res, next) => {
-    const id = req.user.id;
-    
-    await Profile.findOneAndUpdate({ userId:id },{
-        $set:req.body
-    })
-    
-    const profile = await Profile.findOne({ userId:id });
-    
-    if(!profile) {
-        res.status(404);
-        throw new Error("Cannot find a user profile.");
-    }
+  const id = req.user.id;
 
-    res.status(200).json({
-        success: true,
-        data: profile
-    });
+  const profile = await Profile.findOneAndUpdate(
+    { userId: id },
+    { $set: req.body },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  res.status(200).json({ profile });
 });
