4d9a743a2616be88b2e7d999307f907e95bbf2c4
ar2rworld <60049241+ar2rworld@users.noreply.github.com>
2021-10-09 04:20:20 -0700

    fixes #19

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -7,7 +7,6 @@ import { useSocket } from '../../context/useSocketContext';
 import { useHistory } from 'react-router-dom';
 import ChatSideBanner from '../../components/ChatSideBanner/ChatSideBanner';
 import { useEffect } from 'react';
-import ManageBookings from '../../components/Booking/ManageBookings';
 export default function Dashboard(): JSX.Element {
   const classes = useStyles();
 
@@ -32,9 +31,6 @@ export default function Dashboard(): JSX.Element {
       <CssBaseline />
       <Grid item className={classes.drawerWrapper}>
         <ChatSideBanner loggedInUser={loggedInUser} />
-      </Grid>
-      <Grid item xs={9}>
-        <ManageBookings />
       </Grid>
     </Grid>
   );
