709c34f0f8f3b9c21648ddcc8ccd44b755e7b09a
Pursharth Voha <pursharth.vohra@gmail.com>
2021-10-13 18:03:55 -0400

    implement Price Tag component

--- client/src/components/PriceTag/PriceTag.tsx
+++ client/src/components/PriceTag/PriceTag.tsx
@@ -1,9 +1,24 @@
+import { makeStyles } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 import { Grid, Box } from '@material-ui/core';
+import { Fragment } from 'react';
+interface PriceTag {
+  currency?: string;
+  amount: number;
+  time?: string;
+  size: string;
+}
 
-export default function PriceTag(): JSX.Element {
+export default function PriceTag({ currency = '$', amount, time = 'hr', size }: PriceTag): JSX.Element {
   return (
-    <Grid container>
-      <Box color="primary">This is Price Tag</Box>
-    </Grid>
+    <Fragment>
+      {size === 'small' ? (
+        <Typography variant="subtitle2"> {`${currency} ${amount}/${time}`}</Typography>
+      ) : size === 'medium' ? (
+        <Typography variant="h4"> {`${currency} ${amount}/${time}`} </Typography>
+      ) : (
+        <Typography variant="h2"> {`${currency} ${amount}/${time}`} </Typography>
+      )}
+    </Fragment>
   );
 }
