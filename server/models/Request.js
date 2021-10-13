677c3259d4335d6edfd36e11ac22b3fdeff5a1f3
ethan <ethanmoffat34@gmail.com>
2021-10-13 10:37:54 -0700

    Create Request.js

---
+++ server/models/Request.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const requestSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true
+  },
+  sitterId: {
+    type: String,
+    required: true
+  },
+  start: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  end: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  status: {
+    type: String,
+    enum: ["accepted", "declined", "pending"],
+    default: "pending"
+  },
+  paid: {
+    type: Boolean,
+    default: false
+  }
+});
+
+module.exports = Request = mongoose.model("request", requestSchema);
