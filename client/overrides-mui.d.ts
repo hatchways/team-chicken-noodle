0841f67dedac42bde668cf6ff58846ecf76b7793
ar2rworld <60049241+ar2rworld@users.noreply.github.com>
2021-10-19 03:33:19 -0700

    some more fixes #56

--- client/overrides-mui.d.ts
+++ client/overrides-mui.d.ts
@@ -12,10 +12,3 @@ declare module '@material-ui/core/styles/overrides' {
   interface ComponentNameToClassKey extends overridesNameToClassKey {}
   export interface ComponentNameToClassKey extends CustomType {}
 }
-
-type AdditionalProps = {
-  palette: { secondary: { main: string } };
-};
-declare module '@material-ui/styles' {
-  export interface DefaultTheme extends AdditionalProps {}
-}
